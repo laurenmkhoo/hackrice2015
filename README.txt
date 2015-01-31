9bcbe923fafdb2f57507cc6b8daae6726838c538
Cassie Wang <cassie.q.wang@gmail.com>
2015-01-31 14:35:16 -0600

    list doesn't show anything. can see toast messages.

--- README.txt
+++ README.txt
@@ -1,5 +1,6 @@
 README.txt
 
+<<<<<<< Updated upstream
 THINGS THAT WE ARE CONSIDERING IMPLEMENTING:
 
 Initial start menu of app
@@ -7,3 +8,6 @@ Possibly user-customized category?
 Language converter (i.e. sent message to pirate language)
 Default categories of analysis.
 
+=======
+this is our github respository for hackrice2015. we'll be tracking changes to our text stalking app. k thanks bye. 
+>>>>>>> Stashed changes
--- app/src/main/java/com/pbj/teststat/MainActivity.java
+++ app/src/main/java/com/pbj/teststat/MainActivity.java
@@ -2,38 +2,68 @@ package com.pbj.teststat;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+/**
+ * Main Activity. Displays a list of numbers.
+ *
+ * @author itcuties
+ *
+ */
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
+        List<SMSData> smsList = new ArrayList<SMSData>();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        Uri uri = Uri.parse("content://sms/inbox");
+        Cursor c= getContentResolver().query(uri, null, null ,null,null);
+        startManagingCursor(c);
+        System.out.println("cursor c: " + c);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // Read the sms data and store it in the list
+        if(c.moveToFirst()) {
+            Log.d("Line 43", c.getString(c.getColumnIndexOrThrow("body")).toString());
+            for(int i=0; i < c.getCount(); i++) {
+                SMSData sms = new SMSData();
+                sms.setBody(c.getString(c.getColumnIndexOrThrow("body")).toString());
+                sms.setNumber(c.getString(c.getColumnIndexOrThrow("address")).toString());
+                smsList.add(sms);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                c.moveToNext();
+            }
         }
+        c.close();
 
-        return super.onOptionsItemSelected(item);
+        // Set smsList in the ListAdapter
+        setListAdapter(new ListAdapter(this, smsList));
+
     }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        SMSData sms = (SMSData)getListAdapter().getItem(position);
+
+        Toast.makeText(getApplicationContext(), sms.getBody(), /*Toast.LENGTH_LONG*/ 1000).show();
+        Log.d("onListItemClick", sms.getBody());
+
+    }
+
 }
