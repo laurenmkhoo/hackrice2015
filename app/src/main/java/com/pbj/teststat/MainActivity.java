f44d9d7672b972d74c86ff2c94d8a35c4a13e8c6
pjh5 <pjh5@rice.edu>
2015-01-31 17:10:51 -0600

    Added friend and category class

--- app/src/main/java/com/pbj/teststat/MainActivity.java
+++ app/src/main/java/com/pbj/teststat/MainActivity.java
@@ -29,6 +29,14 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Called when the
+     * @param view
+     */
+    public void goToRankings(View view) {
+        startActivity(new Intent(this, Rankings.class));
+    }
+
     public void onMessageButtonClick(View view) {
         Intent intent = new Intent(this, MessageListActivity.class);
         startActivity(intent);
--- app/src/main/java/com/pbj/teststat/Rankings.java
+++ app/src/main/java/com/pbj/teststat/Rankings.java
@@ -1,29 +1,8 @@
 package com.pbj.teststat;
 
-import android.app.Activity;
 import android.app.ListActivity;
-import android.content.Context;
-import android.net.Uri;
-import android.provider.ContactsContract;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.ListAdapter;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 
 public class Rankings extends ListActivity {
@@ -38,54 +17,5 @@ public class Rankings extends ListActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_rankings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    private class MyArrayAdapter extends ArrayAdapter<String> {
-
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        public MyArrayAdapter(Context context, int textViewResourceId,
-                                  List<String> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
-            }
-        }
-
-        @Override
-        public long getItemId(int position) {
-            String item = getItem(position);
-            return mIdMap.get(item);
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
     }
 }
