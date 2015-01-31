53f311356e27e272660d45a772bcd375b0ea6379
pjh5 <pjh5@rice.edu>
2015-01-31 17:14:02 -0600

    Fleshed out person class a tidbit

--- app/src/main/java/com/pbj/teststat/Person.java
+++ app/src/main/java/com/pbj/teststat/Person.java
@@ -3,6 +3,9 @@ package com.pbj.teststat;
 
 import android.provider.ContactsContract;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Hellemn on 1/31/2015.
  */
@@ -10,11 +13,21 @@ public class Person {
 
     private String name; // may be superfluous
     private ContactsContract.Contacts myContact;
-    private Map<Category, Double> stats;
+    private Map<Category, Double> stats = new HashMap<>();
 
 
     public String getName() {
         return name;
+    }
+
+
+    public update(String text) {
+        assert (text != null);
+
+        for (Category cat : stats.keySet()) {
+            stats.put(cat,
+                    stats.getOrDefault(cat, 0) + cat.countOccurances(text));
+        }
     }
 
 
