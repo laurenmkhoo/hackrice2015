f339c3fcbf940959350cfc5b366c228d44eacc22
pjh5 <pjh5@rice.edu>
2015-02-01 01:10:33 -0600

    Added sort functionality to Ranking

--- app/src/main/java/com/pbj/teststat/Rankings.java
+++ app/src/main/java/com/pbj/teststat/Rankings.java
@@ -11,23 +11,16 @@ import android.widget.AdapterView;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class Rankings extends Activity implements OnItemSelectedListener {
-
-    String selectedCategory;
-    Spinner spinner1;
-    private String[] categories = {Person.Category.MOST_PROFANE.name, Person.Category.MOST_VAIN.name,
-           Person.Category.LONGEST_WORD.name,Person.Category.PARTY_ANIMAL.name, Person.Category.THEY_TEXT_MORE.name,
-           Person.Category.THEY_TEXT_LESS.name, Person.Category.LAUGHER.name, Person.Category.KNOW_NOTHING.name,
-           Person.Category.MISS_BASIC.name,Person.Category.LOTS_OF_TEXTS.name, Person.Category.LONG_TEXTS.name};
-
-    private String[] friends =  {"Lauren", "Bao", "Jesse",
-            "Cassie", "Ben", "Phillip"};;
+    public static final String PEOPLE_LIST = "PEOPLE_LIST";
 
     private static HashMap<String, String> descriptions = new HashMap<String, String>();
-
     static {
         descriptions.put(Person.Category.MOST_PROFANE.name, "Definitely does not kiss their mother with their mouth.");
         descriptions.put(Person.Category.MOST_VAIN.name, "I don't think these friends are aware that the Earth revolves around the sun and not them.");
@@ -41,11 +34,27 @@ public class Rankings extends Activity implements OnItemSelectedListener {
         descriptions.put(Person.Category.LOTS_OF_TEXTS.name, "The killer of limited text customers world-wide");
         descriptions.put(Person.Category.LONG_TEXTS.name, "They've probably spent an hour constructing each text");
     }
+
+    private static final String[] categories = new String[Person.Category.values().length];
+    static {
+        int i = 0;
+        for (Person.Category c : Person.Category.values()) {
+            categories[i] = c.name;
+        }
+    }
+
+    String selectedCategory;
+    Spinner spinner1;
+
+    private List<PersonWithRank> friends;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         setContentView(R.layout.activity_rankings);
+
+        // Get friends from intent
+        friends = (List<PersonWithRank>) getIntent().getExtras().get(PEOPLE_LIST);
 
         ArrayAdapter<String> adapter_state = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
         spinner1 = (Spinner) findViewById(R.id.spinner1);
@@ -54,24 +63,27 @@ public class Rankings extends Activity implements OnItemSelectedListener {
 
         final ListView listview = (ListView) findViewById(R.id.listview);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        // Make and set Adapter
+        ArrayAdapter<PersonWithRank> adapter = new ArrayAdapter<PersonWithRank>(this,
                 android.R.layout.simple_list_item_1, friends);
         listview.setAdapter(adapter);
-
-
-    }
-
-    private void orderPersons(){
-        for(int i = 0; i < friends.length; i++){
-
-        }
-
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getApplicationContext(), descriptions.get(parent.getItemAtPosition(position).toString()), Toast.LENGTH_LONG).show();
+        // Display Description
+        Toast.makeText(getApplicationContext(), descriptions.get(
+                parent.getItemAtPosition(position).toString()), Toast.LENGTH_LONG).show();
+
+        // Select Category and Sort Rankings
         selectedCategory = parent.getItemAtPosition(position).toString();
+        Collections.sort(friends, new Comparator<PersonWithRank>() {
+
+            @Override
+            public int compare(PersonWithRank lhs, PersonWithRank rhs) {
+                return (int) (rhs.p.getRatingFor(selectedCategory) - lhs.p.getRatingFor(selectedCategory));
+            }
+        })
     }
 
     @Override
@@ -81,5 +93,19 @@ public class Rankings extends Activity implements OnItemSelectedListener {
 
 
 
+    private class PersonWithRank {
+        public final Person p;
+        public final int rank;
+
+        public PersonWithRank(Person p, int rank) {
+            this.p = p;
+            this.rank = rank;
+        }
+
+
+        public String toString() {
+            return p + "  " + rank;
+        }
+    }
 
 }
