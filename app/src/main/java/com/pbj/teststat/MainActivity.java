ce525672bb82513c940e38085f2e177a1e6dcc81
Phillip Pan <phillip.y.pan@gmail.com>
2015-01-31 21:57:30 -0600

    Styled the main page in activity_main.xml. Added buttonstyle.xml to style multiple buttons at once. Changed app name in string.xml. Added method in MainActivity.java to manipulate multiple xml views at once.

--- app/src/main/java/com/pbj/teststat/MainActivity.java
+++ app/src/main/java/com/pbj/teststat/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pbj.teststat;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -13,7 +14,9 @@ import java.util.List;
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.net.Uri;
+import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -27,11 +30,43 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList <View> views = getViewsByTag((ViewGroup)findViewById(R.id.fun), "button");
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/BLANCH_CONDENSED_LIGHT.otf");
+        for (int i = 0; i < views.size(); i++) {
+            TextView tv = (TextView)(views.get(i));
+            tv.setTypeface(tf);
+        }
+
+        ArrayList <View> viewsBold = getViewsByTag((ViewGroup)findViewById(R.id.fun), "header");
+        Typeface tfBold = Typeface.createFromAsset(getAssets(), "fonts/BLANCH_CONDENSED_INLINE.otf");
+        for (int i = 0; i < viewsBold.size(); i++) {
+            TextView tvBold = (TextView)(viewsBold.get(i));
+            tvBold.setTypeface(tfBold);
+        }
     }
 
     public void onMessageButtonClick(View view) {
         Intent intent = new Intent(this, MessageListActivity.class);
         startActivity(intent);
     }
+
+    private static ArrayList<View> getViewsByTag(ViewGroup root, String tag){
+        ArrayList<View> views = new ArrayList<View>();
+
+        final int childCount = root.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View child = root.getChildAt(i);
+            final int temp_id = child.getId();
+
+            final Object tagObj = child.getTag();
+            if (tagObj != null && tagObj.equals(tag)) {
+                views.add(child);
+            }
+
+        }
+        return views;
+    }
+
 
 }
