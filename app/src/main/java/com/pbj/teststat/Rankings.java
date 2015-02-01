68eaf5c294f74a6d40bd3b85995b8cd486e4d463
Bao Vu <bnv1@rice.edu>
2015-02-01 00:39:42 -0600

    Add descriptions to categories

--- app/src/main/java/com/pbj/teststat/Rankings.java
+++ app/src/main/java/com/pbj/teststat/Rankings.java
@@ -9,6 +9,9 @@ import android.widget.ListView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.AdapterView;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 
 public class Rankings extends Activity implements OnItemSelectedListener {
@@ -23,6 +26,21 @@ public class Rankings extends Activity implements OnItemSelectedListener {
     private String[] friends =  {"Lauren", "Bao", "Jesse",
             "Cassie", "Ben", "Phillip"};;
 
+    private static HashMap<String, String> descriptions = new HashMap<String, String>();
+
+    static {
+        descriptions.put(Person.Category.MOST_PROFANE.name, "Definitely does not kiss their mother with their mouth.");
+        descriptions.put(Person.Category.MOST_VAIN.name, "I don't think these friends are aware that the Earth revolves around the sun and not them.");
+        descriptions.put(Person.Category.LONGEST_WORD.name, "These friends are trying to compensate for something with their large-word-knowing");
+        descriptions.put(Person.Category.PARTY_ANIMAL.name, "This friend is probably most likely to get insurance on their liver");
+        descriptions.put(Person.Category.THEY_TEXT_MORE.name, "You should probably text these people a bit more...");
+        descriptions.put(Person.Category.THEY_TEXT_LESS.name, "I wouldn't be surprised if these people had shrines dedicated to you.");
+        descriptions.put(Person.Category.LAUGHER.name, "They're not actually laughing out loud, just so you know.");
+        descriptions.put(Person.Category.KNOW_NOTHING.name, "If you were on a trivia show, don't bother calling these people");
+        descriptions.put(Person.Category.MISS_BASIC.name, "Don't get too close to these people, I hear basic-ness is contagious");
+        descriptions.put(Person.Category.LOTS_OF_TEXTS.name, "The killer of limited text customers world-wide");
+        descriptions.put(Person.Category.LONG_TEXTS.name, "They've probably spent an hour constructing each text");
+    }
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -52,6 +70,7 @@ public class Rankings extends Activity implements OnItemSelectedListener {
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), parent.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
         selectedCategory = parent.getItemAtPosition(position).toString();
     }
 
