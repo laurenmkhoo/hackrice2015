71848d2a1745d7339ced45956117b96612d30153
laurenmkhoo <lmk9@rice.edu>
2015-01-31 20:18:04 -0600

    Rankings now has dropdown and listview

--- app/src/main/java/com/pbj/teststat/Rankings.java
+++ app/src/main/java/com/pbj/teststat/Rankings.java
@@ -3,19 +3,33 @@ package com.pbj.teststat;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.app.Activity;
+import android.widget.ListView;
 
 
-public class Rankings extends ListActivity {
+public class Rankings extends Activity {
 
+    Spinner spinner1;
+    private String[] names = {"Profane","Vain","Hungry","Party","Slow Response"};
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
+
+        setContentView(R.layout.activity_rankings);
+
+        ArrayAdapter<String> adapter_state = new ArrayAdapter<String>(this,  android.R.layout.simple_spinner_item, names);
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setAdapter(adapter_state);
+
+        final ListView listview = (ListView) findViewById(R.id.listview);
+        String[] values = new String[] { "Lauren", "Bao", "Jesse",
+                "Cassie", "Ben", "Phillip"};
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
+        listview.setAdapter(adapter);
+
     }
 }
